e862ede23b66bb71fff30c09c8689fb27d685530
anderson <anderson@debian.amrconsulting.com>
2018-02-19 13:25:00 -0500

    Ajustó defectos encontrados por el mentor

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,20 @@
 class BlogsController < ApplicationController
+	before_action :find_blog, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@blogs = Blog.all.order(created_at: :desc)
 	end
 
 	def show
-		@blog = Blog.find(params[:id])
+		#@blog = Blog.find(params[:id])
 	end
 
 	def new
-		@blog = Blog.new
+		if user_signed_in?
+			@blog = Blog.new
+		else
+			redirect_to new_user_session_path
+		end
 	end
 
 	def create
@@ -22,11 +27,11 @@ class BlogsController < ApplicationController
 	end
 
 	def edit
-		@blog = Blog.find(params[:id])
+		#@blog = Blog.find(params[:id])
 	end
 
 	def update
-		@blog = Blog.find(params[:id])
+		#@blog = Blog.find(params[:id])
 		if @blog.update(blog_params)
 			redirect_to blogs_path, notice: "El post fue actualizado exitosamente"
 		else
@@ -36,19 +41,20 @@ class BlogsController < ApplicationController
 	end
 
 	def destroy
-		blog = Blog.find(params[:id])
-		if blog.comments.count > 0
-			redirect_to blogs_path, notice: "El post tiene comentarios. No es posible eliminarlo"
-		else
-			if blog.destroy
-				redirect_to blogs_path, notice: "El post fue eliminado exitosamente"
-			end
+		#blog = Blog.find(params[:id])
+		if @blog.destroy
+			redirect_to blogs_path, notice: "El post fue eliminado exitosamente"
 		end
 	end
 
 	protected
 	def blog_params
 		params.require(:blog).permit(:title,:body,:user_id)
+	end
+
+	private
+	def find_blog
+		@blog = Blog.find(params[:id])
 	end
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,6 @@
 class Blog < ApplicationRecord
 	belongs_to :user
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true
 	validates :body, presence: true
 	validates :body, length: {minimum: 250}
